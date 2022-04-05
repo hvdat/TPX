fa46ed80aa51b3c6f2ace2ae0c416f3f2f9f8ad9
DELL\vandat <hovandat0.1@hotmail.com>
2022-04-05 07:29:51 +0700

    connect sql database

---
+++ admin-web/models/users.js
@@ -0,0 +1,76 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('users', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING(256),
+      allowNull: true,
+      unique: "name_UNIQUE"
+    },
+    password: {
+      type: DataTypes.STRING(120),
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING(256),
+      allowNull: true,
+      unique: "email_UNIQUE"
+    },
+    tel: {
+      type: DataTypes.STRING(10),
+      allowNull: true,
+      unique: "sdt_UNIQUE"
+    }
+  }, {
+    sequelize,
+    tableName: 'users',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+      {
+        name: "idusers_UNIQUE",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+      {
+        name: "name_UNIQUE",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "username" },
+        ]
+      },
+      {
+        name: "email_UNIQUE",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "email" },
+        ]
+      },
+      {
+        name: "sdt_UNIQUE",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "tel" },
+        ]
+      },
+    ]
+  });
+};
