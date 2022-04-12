fc827f5c3a702265e64b09190d67a857663b766c
zen-h01 <67859095+zen-h01@users.noreply.github.com>
2022-04-12 15:40:43 +0700

    make small change cuz just reinstall windows xD

--- Customer/models/users.js
+++ Customer/models/users.js
@@ -1,74 +1,53 @@
-const Sequelize = require("sequelize");
-module.exports = function (sequelize, DataTypes) {
-  return sequelize.define(
-    "users",
-    {
-      id: {
-        autoIncrement: true,
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-      },
-      username: {
-        type: DataTypes.STRING(256),
-        allowNull: true,
-        unique: "name_UNIQUE",
-      },
-      password: {
-        type: DataTypes.STRING(120),
-        allowNull: true,
-      },
-      email: {
-        type: DataTypes.STRING(256),
-        allowNull: true,
-        unique: "email_UNIQUE",
-      },
-      tel: {
-        type: DataTypes.STRING(10),
-        allowNull: true,
-        unique: "sdt_UNIQUE",
-      },
-      avatar: {
-        type: DataTypes.STRING(2048),
-        allowNull: true,
-      },
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('users', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
     },
-    {
-      sequelize,
-      tableName: "users",
-      timestamps: false,
-      indexes: [
-        {
-          name: "PRIMARY",
-          unique: true,
-          using: "BTREE",
-          fields: [{ name: "id" }],
-        },
-        {
-          name: "idusers_UNIQUE",
-          unique: true,
-          using: "BTREE",
-          fields: [{ name: "id" }],
-        },
-        {
-          name: "name_UNIQUE",
-          unique: true,
-          using: "BTREE",
-          fields: [{ name: "username" }],
-        },
-        {
-          name: "email_UNIQUE",
-          unique: true,
-          using: "BTREE",
-          fields: [{ name: "email" }],
-        },
-        {
-          name: "sdt_UNIQUE",
-          unique: true,
-          using: "BTREE",
-          fields: [{ name: "tel" }],
-        },
-      ],
+    username: {
+      type: DataTypes.STRING(256),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(120),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(256),
+      allowNull: false
+    },
+    tel: {
+      type: DataTypes.STRING(10),
+      allowNull: true
+    },
+    avatar: {
+      type: DataTypes.STRING(2048),
+      allowNull: true
     }
-  );
+  }, {
+    sequelize,
+    tableName: 'users',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+      {
+        name: "id_UNIQUE",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
 };
