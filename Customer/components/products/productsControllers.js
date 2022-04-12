ad807c2fceeeb916b6e50c0bb6ff46fb396b1c4b
zen-h01 <67859095+zen-h01@users.noreply.github.com>
2022-04-12 16:37:51 +0700

    paging the shop

--- Customer/components/products/productsControllers.js
+++ Customer/components/products/productsControllers.js
@@ -1,9 +1,22 @@
 const { list } = require("./productsServices");
-
+const ITEM_PER_PAGE = 6;
 exports.list = async (req, res, next) => {
-  const products = await list();
-  console.log(products);
-  res.render("product/shop", { products });
+  const { page } = req.query;
+  if (!page || isNaN(page)) page = 1;
+  const { count, rows: products } = await list(
+    (page - 1) * ITEM_PER_PAGE,
+    ITEM_PER_PAGE
+  );
+  const totalPages = Math.ceil(count / ITEM_PER_PAGE);
+  const prevPage = page > 1 ? page - 1 : 1;
+  const nextPage = page < totalPages ? page + 1 : totalPages;
+  res.render("product/shop", {
+    products,
+    count,
+    pages: Array.from(Array(totalPages).keys()).map((i) => i + 1), // [1, 2, 3, 4, 5]
+    prevPage,
+    nextPage,
+  });
 };
 exports.details = async (req, res, next) => {
   res.render("product/single-product");
--- Customer/components/products/productsServices.js
+++ Customer/components/products/productsServices.js
@@ -1,4 +1,4 @@
 const { products } = require("../../models");
-exports.list = async () => {
-  return await products.findAll({ raw: true });
+exports.list = async (offset = 0, limit = 9) => {
+  return await products.findAndCountAll({ raw: true, offset, limit });
 };
