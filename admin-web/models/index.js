5928484d96c8fcedbe4314f432191b6390485466
19127558 <98400325+ShaneIsDev@users.noreply.github.com>
2022-04-24 20:23:59 +0700

    edited database informations

--- admin-web/models/index.js
+++ admin-web/models/index.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize("mysql://root:admin@127.0.0.1/thuc-pham-xanh");
+const sequelize = new Sequelize("mysql://u8ao5ymivydbzh0z:Uoajd0P5Px2A4U0QQGWW@bb76zmr3niir5g6jctnb-mysql.services.clever-cloud.com/bb76zmr3niir5g6jctnb");
 const initModels = require("./init-models");
 
 module.exports = initModels(sequelize);
