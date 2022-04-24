089446f14740d541d97c949910b57d1fe1e57a99
19127558 <98400325+ShaneIsDev@users.noreply.github.com>
2022-04-24 12:21:02 +0700

    Revert "Revert "fixed pagination 1""
    This reverts commit ed14f879bbba94a5cfbdffc64cda2972f2648439.

--- admin-web/components/list_product/listControllers.js
+++ admin-web/components/list_product/listControllers.js
@@ -18,3 +18,22 @@ exports.listShow = async (req, res, next) => {
       nextPage,
     });
 };
+exports.apiList = async (req, res) => {
+    const page = Number(req.params.page);
+    if (!page || isNaN(page)) page = 1;
+    const { count, rows: products } = await list(
+      (page - 1) * ITEM_PER_PAGE,
+      ITEM_PER_PAGE
+    );
+    const totalPages = Math.ceil(count / ITEM_PER_PAGE);
+    const prevPage = page > 1 ? page - 1 : 1;
+    const nextPage = page < totalPages ? page + 1 : totalPages;
+    res.json({
+      products,
+      count,
+      pages: Array.from(Array(totalPages).keys()).map((i) => i + 1), // [1, 2, 3, 4, 5]
+      prevPage,
+      nextPage,
+    });
+  };
+  
